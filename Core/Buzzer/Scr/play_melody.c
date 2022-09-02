77c38a0f8e09a1b9b8f1b428900034a20dde9e46
muharlyamov <mig@promo-bot.ru>
2022-09-02 20:41:23 +0500

    Перенес функции проигрывания мелодии в отдельную функцию

---
+++ Core/Buzzer/Scr/play_melody.c
@@ -0,0 +1,40 @@
+#include "buzzerDriver.h"
+#include "melody.h"
+#include "main.h"
+#include "tim.h"
+
+uint32_t tone_iter = 0;
+volatile uint32_t tone_delay = 20;
+
+void buzzerSetNewFrequency(uint32_t newFreq)
+{
+	uint64_t tempFreq = newFreq;
+	if(newFreq == 0) tempFreq = 1;
+
+	uint64_t tempNewValue = (uint64_t) CPU_FREQ / PRESCALER / tempFreq;
+
+	// setting new value
+	TIM4 ->ARR = (uint32_t)tempNewValue;
+	TIM4 -> CCR4 = (uint32_t)tempNewValue/2;
+
+}
+
+void play_melody() {
+
+  ++tone_iter;
+  uint8_t temp = sizeof(marioMelody)/sizeof(marioMelody[0]);
+  if (tone_iter < temp) {
+	  /*
+	 if(tone_iter % 4){
+		  buzzerSetNewFrequency(test[tone_iter]);
+		  tone_delay = test[tone_iter + 3];
+	  }
+	   */
+	  buzzerSetNewFrequency(marioMelody[tone_iter]);
+	  tone_delay = marioDuration[tone_iter] * melodySlowfactor[0];
+  }
+  else {
+	  tone_delay = 3000;
+	  tone_iter = 0;
+  }
+}
